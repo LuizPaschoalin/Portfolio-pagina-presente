9db7c4b3ddd45fadf396b01c55c26c196c12b2c7
Luiz Paschoalin <luiz.nerf@hotmail.com>
2023-09-29 21:13:12 -0300

    js

--- Scripts/add_image.js
+++ Scripts/add_image.js
@@ -1,20 +1,18 @@
 document.addEventListener('DOMContentLoaded' , function () {
     const inputFile = 
     document.querySelector('.content__firstblock__picture__input');
+    const inputFile2 = 
+    document.querySelector('.content__thirdblock__picture__input');
     const pictureImage = 
     document.querySelector('.content__firstblock__picture__image');
     const pictureImage2 = 
-    document.querySelector('.content__thirdblock__picture__image');
+    document.querySelectorAll('.content__thirdblock__picture__image');
     const pictureImageTxt = 'Choose an image';
     pictureImage.innerHTML = pictureImageTxt;
-    pictureImage2.innerHTML = pictureImageTxt;
-
-    inputFile.addEventListener('change' , function(e) {
-        const inputTarget = e.target;
-        console.log(inputTarget);
-    })
-})
-
+    pictureImage2.forEach(pictureImage2 => {
+        pictureImage2.innerHTML = pictureImageTxt;
+    });
+});
 
    
 
